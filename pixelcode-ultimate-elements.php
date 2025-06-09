3c4f97f4ce4fbfa776a30a7fe44bc6ff7009a10c
rafsanx <developer.rafsanx@gmail.com>
2025-06-09 17:13:50 +0600

    pixelcode-ultimate-elements development

---
+++ pixelcode-ultimate-elements.php
@@ -0,0 +1,13 @@
+/*
+ * Plugin Name:       PixelCode Ultimate Elements
+ * Plugin URI:        https://github.com/developer-rafsan/wordpress-plugin.git
+ * Description:       Pixel Code Ultimate Elementor Addons enhance your Elementor site with customizable widgets, animations, sliders, etc, and regular updates for improved functionality.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            PixelCode
+ * Author URI:        https://portfolio-client-y9gw.onrender.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       pixel-code-ultimate
+ */
